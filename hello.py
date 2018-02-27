3ff58bc87605bc8b775c571489aa57754b4ac4ed
Unknown <bstanton773@gmail.com>
2018-02-27 13:28:21 -0600

    Display list of posts using for loop

--- hello.py
+++ hello.py
@@ -14,6 +14,10 @@ def index():
         {
             'title': {'main': 'I love Python', 'sub': 'this is a happy story'},
             'body': 'Python can do every thing. However, it is very easy'
+        },
+        {
+            'title': {'main': 'I love C#', 'sub': 'this is a happy story'},
+            'body': 'C# can do some things. However, it is very easy'
         }
     ]
     return render_template('index.html', user=user, posts = posts)
