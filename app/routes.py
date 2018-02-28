53b1b1c4219af8e0d05d286119c4d449c49b8382
Unknown <bstanton773@gmail.com>
2018-02-28 14:39:40 -0600

    Update Database with posts

--- app/routes.py
+++ app/routes.py
@@ -2,28 +2,13 @@ from app import app2
 from app.forms import LoginForm
 from flask import render_template, redirect, flash
 
+from app import Post
+
 @app2.route('/')
 @app2.route('/index')
 def index():
     user = {'username': 'Brian'}
-    posts = [
-        {
-            'title': {'main': 'I hate Python', 'sub': 'this is a sad story'},
-            'body': 'Python can do every thing. However, it is very hard'
-        },
-        {
-            'title': {'main': 'I love Python', 'sub': 'this is a happy story'},
-            'body': 'Python can do every thing. However, it is very easy'
-        },
-        {
-            'title': {'main': 'I love C#', 'sub': 'this is a happy story'},
-            'body': 'C# can do some things. However, it is very easy'
-        },
-        {
-            'title': {'main': 'I hate C#', 'sub': 'this is a sad story'},
-            'body': 'C# can do no things. However, it is very stupid!'
-        }
-    ]
+    posts = Post.query.all()
     return render_template('index.html', user=user, posts = posts, title='A title')
 
 @app2.route('/store')
