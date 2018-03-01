e3c323ad8fef9d76b6ea16e36ef7a365627f1dbd
Unknown <bstanton773@gmail.com>
2018-03-01 11:30:57 -0600

    Add login/logout

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app2
 from app.forms import LoginForm
-from flask import render_template, redirect, flash
+from flask import render_template, redirect, flash, url_for
+from flask_login import current_user, login_user, logout_user
 
-from app.models import Post
+from app.models import Post, User
 
 @app2.route('/')
 @app2.route('/index')
@@ -29,10 +30,26 @@ def store():
     ]
     return render_template('store.html', items=items, title='A title 2')
 
-@app2.route('/login', methods = ['GET', 'POST'])
+@app2.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title="Sign In", form=form)
+    if form.validate_on_submit():# post and submit validate
+
+        # get the user from data base use code
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title='Sign In', form=form) # GET or submit validate Flaid
+
+@app2.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
